1ae50e512d80c8e3b995ab64beefbc1d772ed40d
seungki <seungki.1215@gmail.com>
2020-07-14 16:22:24 +0900

    Add header value field each environment

--- policies/header-validation/0.1/header-validation.lua
+++ policies/header-validation/0.1/header-validation.lua
@@ -11,8 +11,11 @@ function _M.new(config)
   local conf = config or {}
 
   self.header_name = conf.header_name
-  self.header_value = conf.header_value
+  self.header_value_staging = conf.header_value_staging
+  self.header_value_production = conf.header_value_production
   self.error_msg = conf.error_msg or default_err_msg
+
+
 
   return self
 end
@@ -20,8 +23,14 @@ end
 function _M:access()
   if self.header_name ~= nil then
     local h_value = ngx.req.get_headers()[self.header_name]
+    local env_server = os.getenv("THREESCALE_DEPLOYMENT_ENV")
+    local header_value = self.header_value_staging
 
-    if h_value ~= self.header_value then
+    if env_server == "production" then
+      header_value = self.header_value_production
+    end
+
+    if h_value ~= header_value then
       ngx.status = ngx.HTTP_UNAUTHORIZED
       ngx.say(self.error_msg)
       ngx.exit(ngx.status)
